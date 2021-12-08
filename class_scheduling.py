27a2a77ebc06328e2726631e7936d71bdcb02ff2
Akinade Mayowa <akinade.mayowa@gmail.com>
2021-12-08 01:04:19 +0100

    added scheduling using genetics algoritm

--- class_scheduling.py
+++ class_scheduling.py
@@ -1,14 +1,146 @@
+import random as rnd
+
+
 class Data:
-    ROOMS = [['R1', 25], ['R2', 45], ['R3', 35]]
+    ROOMS = [['R1', 25], ['R2', 45], ['R3', 35]]    # rooms with number and seatingCapacity
     MEETING_TIMES = [
-        
-    ]
+        ['MT1', 'MWF 09:00 - 10:00'],
+        ['MT2', 'MWF 10:00 - 11:00'],
+        ['MT3', 'TTH 09:00 - 10:30'],
+        ['MT4', 'MWF 10:30 - 12:00']
+    ]   # meeting_times with id and time
+
+    INSTRUCTORS = [
+        ['I1', 'Dr James Web'],
+        ['I2', 'Mr Mike Brown'],
+        ['I3', 'Dr Steve Day'],
+        ['I4', 'Mrs Jane Doe'],
+    ]   # instructors id and name
+
+    def __init__(self):
+        self._rooms = []
+        self._meetingTimes = []
+        self._instructors = []
+
+        # get the list of ROOM numbers and seatingCapacity,
+        # create instances of Room Class
+        # Save Room Instances into rooms data
+        for i in range(0, len(self.ROOMS)):
+            self._rooms.append(Room(self.ROOMS[i][0], self.ROOMS[i][1]))
+
+        for i in range(0, len(self.MEETING_TIMES)):
+            self._meetingTimes.append(MeetingTime(self.MEETING_TIMES[i][0], self.MEETING_TIMES[i][1]))
+
+        for i in range(0, len(self.INSTRUCTORS)):
+            self._instructors.append(Instructor(self.INSTRUCTORS[i][0], self.INSTRUCTORS[i][1]))
+
+        # create course object instances (number, name, instructors, maxNumbOfStudents)
+        course1 = Course('C1', '325K', [self._instructors[0], self._instructors[1]], 25)
+        course2 = Course('C2', '319K', [self._instructors[0], self._instructors[1], self._instructors[2]], 35)
+        course3 = Course('C1', '462K', [self._instructors[0], self._instructors[1]], 25)
+        course4 = Course('C4', '464K', [self._instructors[2], self._instructors[3]], 30)
+        course5 = Course('C5', '360C', [self._instructors[3]], 35)
+        course6 = Course('C6', '303K', [self._instructors[0], self._instructors[2]], 45)
+        course7 = Course('C7', '303L', [self._instructors[1], self._instructors[3]], 45)
+        self._courses = [course1, course2, course3, course4, course5, course6, course7]
+
+        # create dept object instance ( name, courses)
+        dept1 = Department('MATH', [course1, course3])
+        dept2 = Department('ENG', [course2, course4, course5])
+        dept3 = Department('PHY', [course6, course7])
+        self._depts = [dept1, dept2, dept3]
+        self._numberOfClasses = 0
+        for i in range(0, len(self._depts)):
+            self._numberOfClasses += len(self._depts[i].get_courses())
+
+    def get_rooms(self):
+        return self._rooms
+
+    def get_instructors(self):
+        return self._instructors
+
+    def get_courses(self):
+        return self._courses
+
+    def get_depts(self):
+        return self._depts
+
+    def get_meetingTimes(self):
+        return self._meetingTimes
+
+    def get_numberOfClasses(self):
+        return self._numberOfClasses
+
 
 class Schedule:
-    pass
+    def __init__(self):
+        self._data = data
+        self._classes = []
+        self._numberOfConflicts = 0
+        self._fitness = -1
+        self._classNumb = 0
+        self._isFitnessChanged = True
+
+    def get_classes(self):
+        self._isFitnessChanged = True
+        return self._classes
+
+    def get_numberOfConflicts(self):
+        return self._numberOfConflicts
+
+    def get_fitness(self):
+        if self._isFitnessChanged:
+            self._fitness = self.calculate_fitness()
+            self._isFitnessChanged = False
+        return self._fitness
+
+    def initialize(self):
+        depts = self._data.get_depts()
+        for i in range(0, len(depts)):
+            courses = depts[i].get_courses()
+
+            for j in range(0, len(courses)):
+                newClass = Class(self._classNumb, depts[i], courses[j])
+                self._classNumb += 1
+                newClass.set_meetingTime(data.get_meetingTimes()[rnd.randrange(0, len(data.get_meetingTimes()))])
+                newClass.set_room(data.get_rooms()[rnd.randrange(0, len(data.get_rooms()))])
+                newClass.set_instructor(courses[j].get_instructors()[rnd.randrange(0, len(courses[j].get_instructors()))])
+                self._classes.append(newClass)
+        return self
+
+    def calculate_fitness(self):
+        self._numberOfConflicts = 0
+        classes = self.get_classes()
+        for i in range(0, len(classes)):
+            if classes[i].get_room().get_seatingCapacity() < classes[i].get_course().get_maxNumbOfStudents():
+                self._numberOfConflicts += 1
+            for j in range(0, len(classes)):
+                if j >= 1:
+                    if classes[i].get_meetingTime() == classes[j].get_meetingTime() and classes[i].get_id() != classes[j].get_id():
+                        if classes[i].get_room() == classes[j].get_room():
+                            self._numberOfConflicts += 1
+                        if classes[i].get_instructor() == classes[j].get_instructor():
+                            self._numberOfConflicts += 1
+        return 1 / (1.0*self._numberOfConflicts + 1)
+
+    def __str__(self):
+        returnValue = ""
+        for i in range(0, len(self._classes) - 1):
+            returnValue += str(self._classes[i]) + ', '
+        returnValue += str(self._classes[len(self._classes) - 1])
+        return returnValue
+
 
 class Population:
-    pass
+    def __init__(self, size):
+        self._size = size
+        self._data = data
+        self._schedules = []
+        for i in range(0, size):
+            self._schedules.append(Schedule().initialize())
+
+    def get_schedules(self):
+        return self._schedules
 
 class GeneticAlgorithm:
     pass
@@ -27,7 +159,7 @@ class Course:
     def get_name(self):
         return self._name
 
-    def get__instructors(self):
+    def get_instructors(self):
         return self._instructors
 
     def get_maxNumbOfStudents(self):
@@ -130,3 +262,6 @@ class Class:
     def __str__(self):
         return str(self._dept.get_name()) + "," + str(self._course.get_number()) + "," + \
                str(self._room.get_number()) + "," + str(self._instructor.get_id()) + "," + str(self._meetingTime.get_id()) + ","
+
+
+data = Data()
